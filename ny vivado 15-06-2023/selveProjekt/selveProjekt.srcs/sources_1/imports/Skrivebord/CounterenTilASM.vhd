09fb53f088f404dd1e6710dd82f41806b70fb608
Abdala Alhouri <128321033+A-Alhouri@users.noreply.github.com>
2023-06-15 16:32:48 +0200

    Add files via upload

---
+++ ny vivado 15-06-2023/selveProjekt/selveProjekt.srcs/sources_1/imports/Skrivebord/CounterenTilASM.vhd
@@ -0,0 +1,35 @@
+-- Listing 4.11
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+entity CounterenTilASM is
+   generic(
+      N: integer := 8;     -- number of bits
+      M: integer := 60     -- mod-M
+  );
+   port(
+      clk, reset: in std_logic;
+      max_tick: out std_logic;
+      q: out std_logic_vector(N-1 downto 0)
+   );
+end CounterenTilASM;
+
+architecture arch of CounterenTilASM is
+   signal r_reg: unsigned(N-1 downto 0);
+   signal r_next: unsigned(N-1 downto 0);
+begin
+ -- register
+  process(clk,reset)
+  begin
+     if (reset='1') then r_reg <= "00001010";
+     elsif (clk'event and clk='1') then 
+     r_reg <= r_next; end if;
+  end process;
+  -- next-state logic
+  r_next <= "00001010" when r_reg=(M-1) else
+            r_reg + 1;
+            
+  -- output logic
+  q <= std_logic_vector(r_reg);
+  max_tick <= '1' when r_reg=(M-1) else '0';
+end arch;
